cd03aa99c85b15bf7b9bec7d684d9716e81f7f4c
jaimefv23 <jaime.fierros262745@potros.itson.edu.mx>
2026-03-31 19:18:36 -0700

    Pruebitas de Productos
    Pruebas unitarias de los metodos, jalaron bien y en mysql todo se muestra bien!

---
+++ DonLuccionano/RestaurantePersistencia/src/test/java/restaurantepersistencia/ProductoDAOTest.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package restaurantepersistencia;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import restaurantedominio.Producto;
+import restaurantedtos.ProductoDTO;
+
+/**
+ *
+ * @author Jaime
+ */
+public class ProductoDAOTest {
+    
+    private ProductoDAO productoDAO;
+    
+    public ProductoDAOTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        productoDAO = new ProductoDAO();
+    }
+    
+    @Test
+    public void registrarNuevoProducto() {
+        String nombrePrueba = "Pizza Especial"; 
+        ProductoDTO producto = new ProductoDTO(nombrePrueba, 250.50f);
+        
+        assertDoesNotThrow(() -> {
+            Producto productoNuevo = productoDAO.crearProducto(producto);
+            assertNotNull(productoNuevo);
+            assertEquals(nombrePrueba, productoNuevo.getNombre());
+            assertEquals(250.50f, productoNuevo.getPrecio());
+        });
+    }
+    
+    @Test
+    public void buscarParaValidarProductoNombre() {
+        String nombrePrueba = "Hamburguesa Doble";
+        
+        assertDoesNotThrow(() -> {
+            productoDAO.crearProducto(new ProductoDTO(nombrePrueba, 120.0f));
+            Producto productoBuscar = productoDAO.buscarPorNombre(nombrePrueba);
+            assertNotNull(productoBuscar);
+            assertEquals(nombrePrueba, productoBuscar.getNombre());
+        });
+    }
+    
+    @Test
+    public void buscarProductoQueNoExiste() {
+        String nombrePrueba = "Tacos de Therizinosaurus jeje";
+        
+        assertDoesNotThrow(() -> {
+            Producto productoBuscar = productoDAO.buscarPorNombre(nombrePrueba);
+            assertNull(productoBuscar);
+        });
+    }
+    
+}
